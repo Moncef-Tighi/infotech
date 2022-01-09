99fc95fa1702170def11794a3d25b0cf98e6d153
Moncef-Tighi <monceftighiouart@gmail.com>
2022-01-09 20:26:24 +0100

    redisgne complet des tableaux

--- liste_clients.php
+++ liste_clients.php
@@ -7,10 +7,24 @@
         include("HTML/sidebar.html");
 ?>
 <link rel="stylesheet" href="CSS/lister.css" media="screen" type="text/css" />
+<link rel="stylesheet" href="CSS/table.css" media="screen" type="text/css" />
+
 
 
 <body>
     <section class="main_container">
+
+        <h1 class="title">Liste des clients : </h1>
+        <table>
+            <thead>
+                <tr id="header"> 
+                    <th>Nom du client</th>
+                    <th>Prenom du client</th>
+                    <th>Adresse mail </th>
+                    <th>Actions</th>
+                </tr>
+
+            </thead>
         <?php
                 include("model/database.php");
                 $db=db_connect();
@@ -19,17 +33,17 @@
                 $requete->execute();
                 $data=$requete->fetchAll();
                 foreach($data as $entry){            
-                    echo("<div class='container'><ul>
-                        <li><b>Nom du client : </b>".$entry["nom"]. "</li>
-                        <li><b>Prenom du client : </b> ".$entry["prenom"]. " </li>
-                        <li><b>Adresse mail : </b>".$entry["adresse_email"]. " </li>
-                        <li class='actions'><a href='client_form.php'><img src='img/ajouter.svg' class='btn-actions' alt='Ajouter'></a>
-                        <a href='editer_client.php?id=".$entry["id_client"]."'><img class='btn-actions' src='img/editer.svg' alt='Editer'></a>
-                        <a href='model/supprimer.php?type=client&id=".$entry["id_client"]."'> <img class='btn-actions' src='img/supprimer.svg' alt='Supprimer'></a>
-                        </li></ul></div>");
+                    echo("<tr>
+                            <th>".$entry["nom"]. "</th>
+                            <th>".$entry["prenom"]. "</th>
+                            <th>".$entry["adresse_email"]. "</th>
+                            <th><a href='client_form.php'><img src='img/ajouter.svg' class='btn-actions' alt='Ajouter'></a>
+                            <a href='editer_client.php?id=".$entry["id_client"]."'><img class='btn-actions' src='img/editer.svg' alt='Editer'></a>
+                            <a href='model/supprimer.php?type=client&id=".$entry["id_client"]."'> <img class='btn-actions' src='img/supprimer.svg' alt='Supprimer'></a></th>
+                        </tr>");
                 }
             ?>
-
+        </table>
     </section>
 
 
